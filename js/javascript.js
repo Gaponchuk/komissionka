0445f431deb4586e5103ba24f46b3471e6fe2960
Gaponchuk Yuriy <gaponchykyn@gmail.com>
2018-06-09 02:38:40 +0500

    добавил кнопку -вверх-, закончил с js для мобильного меню/ссылок и кнопки вверх

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,35 @@
+const topPage = document.querySelector("#top");
 const nav = document.querySelector(".navigation");
 const btn = document.querySelector(".btn-nav");
-const span = btn.querySelectorAll(".btn-nav__item");
+const btnTop = document.querySelector(".btn-top");
+let span = btn.querySelectorAll(".btn-nav__item");
+let links = nav.querySelectorAll(".navigation__link");
 
-btn.addEventListener("click", ()=>{
+btn.addEventListener("click", ()=> {
+	mobileMenu();
+});
+
+links.forEach(function(link){
+	link.addEventListener("click", ()=> {
+		links.forEach(function(link) {
+			link.classList.remove("active-link");
+		});
+		link.classList.add("active-link");
+		mobileMenu();
+	});
+});
+
+function mobileMenu () {
 	nav.classList.toggle("active");
 	span.forEach(function(i){
 		i.classList.toggle("active");
-	})
+	});
+};
+
+window.addEventListener("scroll", () => {
+	if(window.pageYOffset > 500) {
+		btnTop.classList.add("activ-up");
+	} else {
+		btnTop.classList.remove("activ-up");
+	}
 });
